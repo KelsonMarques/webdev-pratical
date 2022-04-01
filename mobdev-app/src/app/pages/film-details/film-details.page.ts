31e6a107eec3b07133da0004c72e49e6733e7b3b
KelsonMarques <kelsonmarquesbm301@hotmail.com>
2022-04-01 12:58:51 +0000

    Pagination

--- mobdev-app/src/app/pages/film-details/film-details.page.ts
+++ mobdev-app/src/app/pages/film-details/film-details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-film-details',
@@ -9,7 +11,16 @@ export class FilmDetailsPage implements OnInit {
 
   constructor() { }
 
+  film: any;
+ 
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
+
   ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.http.get(`https://swapi.co/api/films/${id}`).subscribe(res => {
+      this.film = res;
+    });
+
   }
 
 }
